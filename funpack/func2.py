c3073b21378958a66182074c39e6d185d97aadb8
Eldiar O <catzilla312@gmail.com>
2017-04-18 16:30:06 +0600

    Adding files for home work 3 on a new branch

---
+++ funpack/func2.py
@@ -0,0 +1,13 @@
+# -- coding: utf-8 --
+def compar2(q):
+
+	if q in range(1,7):
+		return 1
+	elif q in range(7,18):
+		return 2
+	elif q in range(18,25):
+		return 3
+	elif q in range(25,60):
+		return 4
+	elif q in range(60,120):
+		return 5
